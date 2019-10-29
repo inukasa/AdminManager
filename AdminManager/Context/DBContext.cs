bd33bde0d01c8569ce147ce2982e912bf8e34afe
inukasa <34494364+inukasa@users.noreply.github.com>
2019-10-29 23:48:29 +0700

    fdgg
    as

---
+++ AdminManager/Context/DBContext.cs
@@ -0,0 +1,30 @@
+﻿using AdminManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AdminManager.Context
+{
+    public class DBContext
+    {
+        public List<UserLoginModels> getAllUserLogin(){
+            List<UserLoginModels> listULogin = new List<UserLoginModels>();
+            string connString = ConfigurationManager.ConnectionStrings["stringCapstone"].ConnectionString;
+            SqlConnection con = new SqlConnection(connString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select Username, Passwords from Users",con);
+            SqlDataReader rd = cmd.ExecuteReader();
+            while (rd.Read())
+            {
+                UserLoginModels user = new UserLoginModels(rd.GetValue(0).ToString(), rd.GetValue(1).ToString());
+                listULogin.Add(user);
+            }
+            con.Close();
+            return listULogin;
+        }
+    }
+}
---
+++ AdminManager/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using AdminManager.Context;
+using AdminManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace AdminManager.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserLoginModels user, string returnURL)
+        {
+            if (IsValid(user))
+            {
+                FormsAuthentication.SetAuthCookie(user.username, false);
+                return Redirect(returnURL);
+            }
+            else
+            {
+                return View(user);
+            }
+            
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("/Home/Index");
+        }
+        private bool IsValid(UserLoginModels user)
+        {
+            DBContext db = new DBContext();
+            List<UserLoginModels> listUL = db.getAllUserLogin();
+            bool flag = false;
+            foreach(UserLoginModels element in listUL)
+            {
+                if(user.username == element.username && user.password == element.password)
+                {
+                    flag = true;
+                }
+            }
+            return flag;
+        }
+    }
+}
